dad45d596e18d3eb9fa5b19a564bd17772c8e190
kaoru <k@warpnine.io>
2025-02-17 17:31:23 +0900

    refactor: simplify internal

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use anyhow::Result;
-use data_uri_converter::DataUriConverter;
+use clap::Parser;
 
-fn main() -> Result<()> {
-    println!("{}", DataUriConverter::try_new()?.convert()?);
+#[derive(Parser)]
+#[command(version, about)]
+pub struct Args {
+    /// Path to a file to convert. If text, assume UTF-8.
+    #[arg()]
+    pub file: std::path::PathBuf,
+
+    /// MIME type. If none specified, will determine automagically.
+    #[arg(short, long)]
+    pub mime_type: Option<String>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let Args { ref file, mime_type } = Args::parse();
+    println!("{}", data_uri_converter::DataUriConverter::from_file(file, mime_type)?.convert()?);
 
     Ok(())
 }
